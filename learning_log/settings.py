01196e292e88893f1071535f667ab811eba48353
bogdan <bogdan_manafaka@.mail.ru>
2022-12-07 20:57:58 +0300

    modified:   learning_log/settings.py 	modified:   learning_log/urls.py 	new file:   templates/404.html

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_sy&!50qv73qp$xpdtuarfz5ocf3(l2a-_hpjtpa@(irzt#%70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('', include('learning_logs.urls')),
     path('users/', include('users.urls')),
 ]
+
